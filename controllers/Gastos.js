f0b1daf4de76ae8fed97264bee82e8b1600b6ef8
Dany Bechara <bechara611@gmail.com>
2023-07-27 13:30:19 -0400

    version 8.0 del develop

--- controllers/Gastos.js
+++ controllers/Gastos.js
@@ -1,5 +1,6 @@
 import { request, response } from "express";
 import GastosModelo from "../models/Gastos.js";
+import { fromUnixTime, getUnixTime } from "date-fns";
 
 export const getGastos = async (req = request, res = response) => {
     const gastosBD = await GastosModelo.find().populate('usuario');
@@ -41,7 +42,10 @@ export const PostGastos = async (req = request, res = response) => {
 
     try {
         const { nombre, total, categoria, fecha, fechaUnix, usuario } = req.body
+        console.log(fromUnixTime(fechaUnix))
         const GastoRegistrado = new GastosModelo({ nombre, total, categoria, fecha, fechaUnix, usuario })
+
+
         const respues = await GastoRegistrado.save();
         return res.status(200).json({
             ok: true,
@@ -62,13 +66,23 @@ export const PostGastos = async (req = request, res = response) => {
     }
 }
 
-export const DeleteGasto = (req = request, res = response) => {
+export const DeleteGasto = async (req = request, res = response) => {
     const { id } = req.params
+
+    const existe = await GastosModelo.findById(id)
+    if(!existe){
+        return res.status(400).json({ok:false,
+        msg:'ID NOT FOUND'})
+    }
+
+    const GastoBorrado = await GastosModelo.findByIdAndDelete(id)
+    const gastos = await GastosModelo.find();
     try {
         return res.status(200).json({
             ok: true,
-            msg: 'DeleteGasto',
-            id
+            msg: 'Success',
+            GastoBorrado,
+            gastos
         })
 
     } catch (error) {
